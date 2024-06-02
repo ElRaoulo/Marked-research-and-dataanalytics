3ac10a0d03e8c3c4144a50c1a3fdefa7789c8bd0
Raoul Doerfer <raould97@googlemaile.com>
2024-06-02 13:23:22 +0200

    ergänzung Wertigkeit, einzeluntersuchung Jacke

--- PreteseAnalyse.R
+++ PreteseAnalyse.R
@@ -44,5 +44,70 @@ liking_mean <- reduce_columns(data_outfits_liking)[,37:48] %>%
 #overview of Konsistenz and Liking of the outfits
 konsistenzXliking <- merge(konsistenz_mean, liking_mean, by = "row.names", all = TRUE)
 
+colnames(konsistenzXliking) <- c("Index", "konsistenz", "liking")
 
+
+data_outfits_wertigkeit <- data_outfits[, grepl("Wertigkeit", names(data_outfits))]
+
+wertigkeit_mean <- reduce_columns(data_outfits_wertigkeit)[,37:48] %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  t()
+
+wertigkeit_mean <-  rownames_to_column(data.frame(wertigkeit_mean), var = "Index")
+
+konsistenzXlikingXwertigkeit <- merge(konsistenzXliking, wertigkeit_mean, by = "Index", all = TRUE)
+
+
+
+
+#Die einzelnen Produkte
+data_jacke <- data[, grepl("Jacke", names(data))]
+
+#hippie2
+data_jacke_hippie2 <- data[, grepl("Jacke_Hippie2", names(data))]
+
+jacke_hippie2_preiserwartung <- data_jacke_hippie2[, grepl("Preiserwartung", names(data_jacke_hippie2))]
+
+
+jacke_hipiie2_mean <- data_jacke_hippie2 %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  t()
+
+
+#min2
+data_jacke_min2 <- data[, grepl("Jacke_Min2", names(data))]
+
+jacke_min2_mean <- data_jacke_min2 %>%
+  summarise(across(everything(), mean, na.rm = TRUE)) %>%
+  t()
+
+
+
+ubersicht <- cbind(jacke_hipiie2_mean, jacke_min2_mean)
+
+
+
+
+
+ggplot(jacke_hippie2_preiserwartung, aes())
+
+
+
+
+
+
+
+
+
+
+# Boxplot erstellen
+ggplot(Jacke_data_filtered, aes(x = Product_type, y= Komplexität, fill=fill_color)) +
+  geom_boxplot(position = position_dodge(width = 0.75)) + 
+  geom_point(stat = "summary", fun = mean, size = 3, shape = 18, color = "red") + 
+  scale_fill_manual(values = c("1" = "grey", "2" = "#00CED1")) +
+  labs(x = "Product Type", 
+       y = "Komplexität (1= 'sehr komplex', ... , 5='gar nicht komplex')", 
+       title = "Boxplot der Komplexitätswahrnehmung Jacken") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
